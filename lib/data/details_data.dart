dc85071b3895c3f2d18f60c96761182693aac265
Ganesh Tamang <tamangganesh@hotmail.com>
2024-03-17 22:38:37 +0545

    deleted

---
+++ lib/data/details_data.dart
@@ -0,0 +1,14 @@
+import 'package:new_cv_for_web/models/person_details_models.dart';
+
+class DetailsData {
+  var myDetails = PersonDetailModel(
+    firstName: "Ganesh",
+    lastName: "Tamang",
+    email: "tamangganesh@hotmail.com ",
+    address: "kapan,Kathmandu,Nepal",
+    phoneNumber: "9808241778",
+    education: [],
+    experience: [],
+    someMomentosPhotos: [],
+  );
+}
